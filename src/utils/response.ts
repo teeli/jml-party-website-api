ad0d11d6b60ea6b22650f9ed54243a57ce9e85d6
Teemu Lahti <teemu.lahti@gmail.com>
2024-03-04 16:21:46 +0200

    feat: add some basic cors and cache control settings

--- src/utils/response.ts
+++ src/utils/response.ts
@@ -1,6 +1,18 @@
-export const generateResponse = (body: string, statusCode: number = 200) => {
-  return {
-    statusCode,
-    body,
-  }
-}
+import { addSeconds, formatRFC7231 } from 'date-fns'
+
+const DEFAULT_TTL = 300
+
+export const generateResponse = (
+  body: string,
+  statusCode: number = 200,
+  customHeaders?: Record<string, string>,
+) => ({
+  statusCode,
+  body,
+  headers: {
+    Expires: formatRFC7231(addSeconds(new Date(), DEFAULT_TTL)),
+    'Content-Type': 'application/json',
+    'Cache-Control': `public, max-age=${DEFAULT_TTL}, s-maxage=${DEFAULT_TTL}, proxy-revalidate`,
+    ...customHeaders,
+  },
+})
