60f90bd8aea4f00c9f5aa215a5e50d0b79cb3a4c
Teemu Lahti <teemu.lahti@gmail.com>
2024-04-11 17:57:58 +0300

    feat: add middy with some default middlewares (#8)

--- src/functions/visitors.ts
+++ src/functions/visitors.ts
@@ -1,26 +1,16 @@
-import { getSpreadSheet } from '../data-sources/sheets'
-import { BaseError } from '../errors/errors'
-import { mapVisitors } from '../mappers/visitor-mapper'
-import { generateResponse } from '../utils/response'
+import middy from '@middy/core'
 
-export const handler = async () => {
+import { getSpreadSheet } from '../data-sources/sheets'
+import { mapVisitors } from '../mappers/visitor-mapper'
+import { defaultMiddlewares } from '../utils/middleware'
+
+const handlerFunction = async () => {
   const sheet = await getSpreadSheet({
     spreadsheetId: process.env.SHEET_ID,
     range: process.env.SHEET_NAME,
   })
 
-  try {
-    const visitors = mapVisitors(sheet)
-
-    return generateResponse(JSON.stringify(visitors))
-  } catch (error) {
-    // output error message of known errors and exist
-    const { knownError, message, statusCode } = error as BaseError
-    if (knownError) {
-      return generateResponse(message, statusCode)
-    }
-
-    // throw unknown errors
-    throw error
-  }
+  return mapVisitors(sheet)
 }
+
+export const handler = middy().use(defaultMiddlewares).handler(handlerFunction)
